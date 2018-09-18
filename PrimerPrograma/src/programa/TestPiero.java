776b9bc9b789249a4974605ef6e1a1478fbcd2cf
Piero <piero.vasquez.r@gmail.com>
2018-09-18 20:28:17 +0200

    Ordenar array de mayor a menor completo

--- PrimerPrograma/src/programa/TestPiero.java
+++ PrimerPrograma/src/programa/TestPiero.java
@@ -1,9 +1,22 @@
 package programa;
 
+import java.util.Arrays;
+
 public class TestPiero {
 	
-	//
+	//Metodo que ordene un array numérico de mayor a menor sin importar el tamaño
 
+	private static void ordenarArray (int[] ord) {
+		for (int i = 0; i < ord.length - 1; i++) {
+			for (int j = i + 1; j < ord.length; j++) {
+				if(ord[i] < ord[j]) {
+					int aux = ord[i];
+					ord[i] = ord[j];
+					ord[j] = aux;
+				}
+			}
+		}
+	}
 	//Metodo que reciba un array numérico e indique si los numeros que lo componen esta ordenados de forma creciente, descendiente, se encuentrar revueltos o son iguales.
 	
 	public static String comprobarArray(int[] arr) {
@@ -34,10 +47,15 @@ public class TestPiero {
 		int[] arr3 = {1,1,1,1,1};
 		int[] arr4 = {5,1,10,-1};
 		
+		int[] ordenados = {1,5,3,9,10};
+		
 		System.out.println(comprobarArray(arr));
 		System.out.println(comprobarArray(arr2));
 		System.out.println(comprobarArray(arr3));
 		System.out.println(comprobarArray(arr4));
+		
+		ordenarArray(ordenados);
+		System.out.println(Arrays.toString(ordenados));
 	}
 
 }

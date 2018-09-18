01171035c7958767ba6139b7052c1a42d9cb480e
Piero <piero.vasquez.r@gmail.com>
2018-09-18 21:06:00 +0200

    Numero perfecto completo

--- PrimerPrograma/src/programa/TestPiero2.java
+++ PrimerPrograma/src/programa/TestPiero2.java
@@ -5,10 +5,21 @@ public class TestPiero2 {
 	//Metodo que indique si un numero es perfecto, un numero es perfecto si la suma de sus divisores(sin contarse a sí mismo) es el mismo numero.
 	//Ejemplo. 6->3,2,1. La suma de 3+2+1=6. POr lo tanto 6 es un número perfecto.
 
+	private static String comprobarPerfecto(int num) {
+		
+		int sum = 0;
+		for(int aux = num - 1; aux > 0; aux--) {
+			if(num % aux == 0) {
+				sum += aux;
+			}
+		}
+		
+		return sum == num ? "Perfecto" : "Común";
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(comprobarPerfecto(6));
 	}
 
 }
